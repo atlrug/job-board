66ef97d657946e1380f9a18522091bd2e8aacc12
vanstee <patrick@vanstee.me>
2014-02-19 23:04:56 -0500

    Install and generate ember app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require job_board
+
+// for more details see: http://emberjs.com/guides/application/
+JobBoard = Ember.Application.create();
+
 //= require_tree .
---
+++ app/assets/javascripts/router.js
@@ -0,0 +1,5 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+JobBoard.Router.map(function() {
+  // this.resource('posts');
+});
